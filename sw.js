bd5954a43ede35bd21a7de0be5272756045a350a
fimenten <41468725+fimenten@users.noreply.github.com>
2025-06-02 13:10:30 +0900

    Remove PWA icons

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'cray-cache-v1';
+const URLS = [
+  './',
+  './dist/bundle.js',
+  './src/styles.css',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
